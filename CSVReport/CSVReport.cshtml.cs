54d47dfde263157ca3330581c04a1c9014c82d32
Tito <Tito@DESKTOP-868MOLJ>
2023-04-05 08:19:46 -0400

    Add project files.

---
+++ CSVReport/CSVReport.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ireport.Idhl
+{
+    public class CSVReportModel : PageModel
+    {
+    }
+}
---
+++ Idhl/ScoreCardReport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ireport.Idhl
+{
+    public class ScoreCardReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
